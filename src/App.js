7c1cd54ebae5b65851551b1046d86682aa743d2c
jet2018 <ezrajet9@gmail.com>
2020-11-19 11:10:38 +0300

    finished home but still with body showing

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Login from './components/auth/Login';
+import ForgotPassword from './components/auth/Forgot_password';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ResetPassword from './components/auth/Reset_password';
+import Page_404 from './Page404';
+
+class App extends Component{
+
+  // componentDidMount(){
+  //   setTimeout(()=> Splash(), 10000);
+  // }
+
+  render(){
+    return(
+      <>
+      <Router>
+        <Switch>
+          <Route path="/reset/:token" component={ResetPassword}/>
+          <Route path="/forgot" component={ForgotPassword}/>
+          <Route path="/" exact component={Login}/>
+          <Route path="/" component={Page_404}/>
+        </Switch>
+      </Router>
+        
+      </>
+    );
+  }
+}
+
+
+export default App;
